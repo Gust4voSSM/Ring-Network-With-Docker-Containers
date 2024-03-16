5a01811711e0c362f7dbaacae2de36e73837f8fe
MusicalUsagi <tavaresbezerra4@gmail.com>
2024-03-16 20:59:22 -0300

    pequeno ajuste

--- Hosts/symmetric.py
+++ Hosts/symmetric.py
@@ -5,17 +5,17 @@ def symmetric_key_encrypt(data, shared_key):
     data = data.encode("utf-8")
     cipher = AES.new(shared_key, AES.MODE_EAX)
     cipher_text, tag = cipher.encrypt_and_digest(data)
-    return [cipher_text, shared_key, cipher.nonce, tag]
+    return [cipher_text, cipher.nonce, tag]
 
-def symmetric_key_decrypt(infos):
-    cipher = AES.new(infos[1], AES.MODE_EAX, infos[2])
-    data = cipher.decrypt_and_verify(infos[0], infos[3])
+def symmetric_key_decrypt(infos, shared_key):
+    cipher = AES.new(shared_key, AES.MODE_EAX, infos[1])
+    data = cipher.decrypt_and_verify(infos[0], infos[2])
     return data.decode("utf-8") 
 
-"""
+
 frase = "testando se tá pegando.com jujuba carro moto 192.32.4.5.1"
-inf = symmetric_key_encrypt(frase)
-print(f"Cifra: {inf[0]}, Chave: {inf[1]}, Nonce: {inf[2]} e Tag: {inf[3]}\n")
-nova_frase = symmetric_key_decrypt(inf)
+key_shared = get_random_bytes(16)
+inf = symmetric_key_encrypt(frase, key_shared)
+print(f"Cifra: {inf[0]}, Nonce: {inf[1]} e Tag: {inf[2]}\n")
+nova_frase = symmetric_key_decrypt(inf, key_shared)
 print(nova_frase)
-"""
