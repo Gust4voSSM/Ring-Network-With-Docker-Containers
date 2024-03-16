f0c3a6a3069ce6781de9aaac4eba949a5c1f1b9a
MusicalUsagi <tavaresbezerra4@gmail.com>
2024-03-16 19:49:51 -0300

    Criação do arquivo responsável pela chave simétrica e adição de uma lib nos requerimentos.txt

---
+++ Hosts/symmetric.py
@@ -0,0 +1,22 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+def symmetric_key_encrypt(data):
+    data = data.encode("utf-8")
+    key = get_random_bytes(32)
+    cipher = AES.new(key, AES.MODE_EAX)
+    ciphertext, tag = cipher.encrypt_and_digest(data)
+    return [ciphertext, key, cipher.nonce, tag]
+
+def symmetric_key_decrypt(infos):
+    cipher = AES.new(infos[1], AES.MODE_EAX, infos[2])
+    data = cipher.decrypt_and_verify(infos[0], infos[3])
+    return data.decode("utf-8") 
+
+"""
+frase = "testando se tá pegando.com jujuba carro moto 192.32.4.5.1"
+inf = symmetric_key_encrypt(frase)
+print(f"Cifra: {inf[0]}, Chave: {inf[1]}, Nonce: {inf[2]} e Tag: {inf[3]}\n")
+nova_frase = symmetric_key_decrypt(inf)
+print(nova_frase)
+"""
