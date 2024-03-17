ea5e698d576274dfd44d4dc30e3481b9ea9272d0
MusicalUsagi <tavaresbezerra4@gmail.com>
2024-03-16 23:31:24 -0300

    Diffie_hellmean feito

--- Hosts/symmetric.py
+++ Hosts/symmetric.py
@@ -1,21 +1,39 @@
 from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
+from socket import socket
+from random import randint
+from sympy import randprime
 
 def symmetric_key_encrypt(data, shared_key):
     data = data.encode("utf-8")
     cipher = AES.new(shared_key, AES.MODE_EAX)
     cipher_text, tag = cipher.encrypt_and_digest(data)
     return [cipher_text, cipher.nonce, tag]
+    #Retorna uma lista que deve ser enviada para o outro lado
 
 def symmetric_key_decrypt(infos, shared_key):
     cipher = AES.new(shared_key, AES.MODE_EAX, infos[1])
     data = cipher.decrypt_and_verify(infos[0], infos[2])
     return data.decode("utf-8") 
+    #Descriptografa a lista recebida do outro lado e retorna a mensagem em texto
+
+def diffie_hellman(socke):
+    random_1 = randprime(1, 2000)
+    socke.send(str(random_1).encode())
+    random_2 = int(socke.recv(2048).decode())
+    if (random_1 > random_2):
+        random_1, random_2 = random_2, random_1
+ 
+    private_key = randint(1, 2000)
+    public_key = random_1**private_key % random_2
+    print("...") #ver esse "bug"
+
+    socke.send(str(public_key).encode())
+    other_public_key = int(socke.recv(2048).decode())
+    
+    shared_key = other_public_key**private_key % random_2
+    shared_key = shared_key.to_bytes(16, 'big')
+
+    return shared_key
+    #Faz o Diffe-hellman e retorna a chave compartilhada que deve ser usada para descriptografar e criptografar mensagens
 
 
-frase = "testando se tá pegando.com jujuba carro moto 192.32.4.5.1"
-key_shared = get_random_bytes(16)
-inf = symmetric_key_encrypt(frase, key_shared)
-print(f"Cifra: {inf[0]}, Nonce: {inf[1]} e Tag: {inf[2]}\n")
-nova_frase = symmetric_key_decrypt(inf, key_shared)
-print(nova_frase)
