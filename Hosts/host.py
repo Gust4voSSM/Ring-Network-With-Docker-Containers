2290e1281724b4c5c31156afb42691416a34f42d
MusicalUsagi <tavaresbezerra4@gmail.com>
2024-03-21 17:48:54 -0300

    Funcionando com Threads, falta criptografia

--- Hosts/host.py
+++ Hosts/host.py
@@ -3,10 +3,38 @@ from app import App
 from threading import Thread
 from os import getenv
 
+mensagem = ""
+
+def enviando(app : App):
+    global mensagem 
+    mensagem = input("")
+
+    while mensagem != "sair":
+        quem = input("Para qual host quer mandar? ")
+        app.send_message_to(mensagem, host[quem])
+        mensagem = input("")
+        
+    
+def recebendo_atras(app : App):
+    global mensagem
+
+    while mensagem != "sair":
+        message = app.receive_message_prev()
+        if message != "FOWARDING":
+            print(f"-Mensagem recebida: {message}")
+
+def recebendo_frente(app : App):
+    global mensagem
+
+    while mensagem != "sair":
+        message = app.receive_message_next()
+        if message != "FOWARDING":
+            print(f"\n-Mensagem recebida: {message}")
+
+
 id = int(getenv("ID"))
 assert id > 0
 host = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6}
-
 
 PREV, NEXT, SRC, DST = 2*[*range(2)]
 
@@ -25,19 +53,17 @@ Seus vizinhos: {'e '.join(ip[1])}\
 
 app = App(id, (ip[SRC][PREV], ip[SRC][NEXT]), (ip[DST][PREV], ip[DST][NEXT]), "192.167.423")
 wait = input("Enter to start")
-mensagem = ""
 
-if(id == host['A']):
-    mensagem = "Olá, a mensagem chegou?"
-    app.send_message_to(mensagem, host['C'])
-    print(f"mensagem enviada {mensagem}")
+enviar = Thread(target=enviando, args=[app])
+receber_1 = Thread(target=recebendo_frente, args=[app])
+receber_2 = Thread(target=recebendo_atras, args=[app])
 
-elif (id == host['B']):
-    mensagem = app.receive_message_prev()
-    print(f"mensagem recebida {mensagem}")
+enviar.start()
+receber_1.start()
+receber_2.start()
 
-elif (id == host['C']):
-    mensagem = app.receive_message_prev()
-    print(f"mensagem recebida {mensagem}")
 
+enviar.join()
+receber_1.join()
+receber_2.join()
 app.kill_both()
