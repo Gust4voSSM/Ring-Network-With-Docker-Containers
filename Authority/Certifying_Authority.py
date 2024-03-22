a015673ce012ef685eacd872d13fe45fd6a4cdce
MusicalUsagi <tavaresbezerra4@gmail.com>
2024-03-21 21:02:45 -0300

    Esqueleto do CA feito

---
+++ Authority/Certifying_Authority.py
@@ -0,0 +1,36 @@
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from socket import socket, AF_INET, SOCK_DGRAM
+import os
+
+SERVER_PORT = 8000
+
+# Simulação da Autoridade Certificadora
+class CertifyingAuthority:
+    def __init__(self, ip : str):
+        self.nodes_public_keys = {}  # Armazena as chaves públicas dos nós
+        self.socket = socket(AF_INET, SOCK_DGRAM)
+        self.socket.bind((ip, SERVER_PORT))
+
+    def generate_keys_for_node(self, node_id):
+        # Geração do par de chaves RSA
+        private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+        public_key = private_key.public_key()
+
+        # Exporta a chave privada
+        private_key_pem = private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()  # Idealmente, deve-se usar uma senha aqui
+        )
+
+        # Armazena a chave pública com o identificador do nó
+        self.nodes_public_keys[node_id] = public_key
+
+        return private_key_pem, public_key
+
+    def get_public_key(self, node_id):
+        return self.nodes_public_keys.get(node_id)
+
